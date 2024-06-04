d4eee725777aa37b12bd73baefd8572ba907c20d
Mert Koç <mertkoc1981@gmail.com>
2024-06-04 15:39:36 +0300

    Dal and bl structure used

---
+++ HotelProject.BusinnesLayer/Abstract/IRoomService.cs
@@ -0,0 +1,15 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinnesLayer.Abstract
+{
+    public interface IRoomService : IGenericService<Room>
+    {
+        
+    }
+}
