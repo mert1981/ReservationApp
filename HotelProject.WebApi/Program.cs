8fee6f176ef8c4ed2ae53508997775baaab408da
Mert Koç <mertkoc1981@gmail.com>
2024-06-05 10:43:57 +0300

    swagger api

--- HotelProject.WebApi/Program.cs
+++ HotelProject.WebApi/Program.cs
@@ -1,3 +1,9 @@
+using HotelProject.BusinnesLayer.Abstract;
+using HotelProject.BusinnesLayer.Concrete;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Context>();
+
+builder.Services.AddScoped<IServicesDal,EfServiceDal>();
+builder.Services.AddScoped <IServiceService,ServicesManager>();
+
+builder.Services.AddScoped<IStaffDal, EfStaffDal>();
+builder.Services.AddScoped<IStaffService, StaffManager>();
+
+builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
+
+builder.Services.AddScoped<IRoomDal, EfRoomDal>();
+builder.Services.AddScoped<IRoomService, RoomManager>();
+
+builder.Services.AddScoped<ISubscribeDal, EfSubscribeDal>();
+builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
+
 
 var app = builder.Build();
 
